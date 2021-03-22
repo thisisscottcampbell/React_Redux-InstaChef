5a1cb1c84d53265f1005396e055bb57b95cdfb6a
Scott Campbell <thisisscottcampbell@gmail.com>
2021-03-21 22:38:27 -0400

    done

--- chef-app/src/components/RecipeList.js
+++ chef-app/src/components/RecipeList.js
@@ -3,24 +3,31 @@ import RecipeItem from './RecipeItem';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
-const RecipeList = ({ recipes }) => {
+const RecipeList = ({ recipes, favorites }) => {
 	return (
 		<div>
-			<p>
-				<Link className="link" to="/favorites">
-					View Favorites
-				</Link>
-			</p>
-			<h4>Search Results</h4>
-			{recipes.map((recipe, i) => (
-				<RecipeItem recipe={recipe} key={i} star={true} />
-			))}
+			{favorites.length > 0 && (
+				<p>
+					<Link className="link" to="/favorites">
+						View Favorites
+					</Link>
+				</p>
+			)}
+
+			{recipes.length > 0 && (
+				<>
+					<h4>Search Results</h4>
+					{recipes.map((recipe, i) => (
+						<RecipeItem recipe={recipe} key={i} star={true} />
+					))}
+				</>
+			)}
 		</div>
 	);
 };
 
 const mapStateToProps = (state) => {
-	return { recipes: state.recipes };
+	return { recipes: state.recipes, favorites: state.favorites };
 };
 
 export default connect(mapStateToProps, null)(RecipeList);
